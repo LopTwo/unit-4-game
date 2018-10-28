ae31cea64d5d1579ef1077d704813353e36e6a72
LopTwo <kfm1@email.arizona.edu>
2018-10-27 18:40:32 -0700

    final commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,12 +2,27 @@ var random_result;
 var lost;
 var win;
 
-for(var i = 0; i < 4; i++){
+random_result = Math.floor(Math.random() * 69 ) + 30;
 
+    
+
+
+$("#result").html('Random Result: ' + random_result);
+
+for (var i = 0; i < 4; i++) {
+
+    var random = Math.floor(Math.random() * 11) + 1;
+    console.log(random);
+
+    
     var crystal = $("<div>");
-        crystal.attr("class", 'crystal');
+    crystal.attr({
+        "class": 'crystal',
+        "data-random": random
+    });
+
     $(".crystals").append(crystal);
 
-  
+
 
 }
